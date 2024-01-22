775af252ded4380852dedb78a5da5bc2c984c84c
jslit <jslit@jungsoo080ui-MacBookPro.local>
2024-01-22 12:20:09 +0900

    add ackermann

--- js/page-main-section/code-page-subpage/recursion-subpage-main.js
+++ js/page-main-section/code-page-subpage/recursion-subpage-main.js
@@ -21,6 +21,16 @@
 // SOFTWARE.
 
 const recursionSubpageItem = {
+    "Ackermann":
+        `function ackermann(m, n) {
+    if (m == 0) {
+        return n + 1;
+    } else if (n == 0) {
+        return ackermann(m - 1, 1);
+    } else {
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
+}`,
     "Capitalize First":
         `function capitalizeFirst(arrayOfStrings) {
     let newArray = [];
